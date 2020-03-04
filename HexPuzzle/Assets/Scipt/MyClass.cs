cd1f5167be5195f26930e6f9bebac22854d09c65
enter_maintenence <eobub15@gmail.com>
2020-03-05 03:17:27 +0900

    [DEV] update mouse move
    - 마우스를 이용한 이동 개발

---
+++ HexPuzzle/Assets/Scipt/MyClass.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eStatus
+{
+    NULL,
+    RED,
+    GREEN,
+    BLUE,
+    MAGENTA,
+    YELLOW,
+    SILVER
+}
+
+[System.Serializable]
+public class MyPoint
+{
+    public int x;
+    public int y;
+
+    public MyPoint(int nx, int ny)
+    {
+        x = nx;
+        y = ny;
+    }
+
+    public void mult(int m)
+    {
+        x *= m;
+        y *= m;
+    }
+
+    public void add(MyPoint p)
+    {
+        x += p.x;
+        y += p.y;
+    }
+
+    public Vector2 ToVector()
+    {
+        return new Vector2(x, y);
+    }
+
+    public bool Equals(MyPoint p)
+    {
+        return (x == p.x && y == p.y);
+    }
+
+    public static MyPoint fromVector(Vector2 v)
+    {
+        return new MyPoint((int)v.x, (int)v.y);
+    }
+
+    public static MyPoint fromVector(Vector3 v)
+    {
+        return new MyPoint((int)v.x, (int)v.y);
+    }
+
+    public static MyPoint mult(MyPoint p, int m)
+    {
+        return new MyPoint(p.x * m, p.y * m);
+    }
+
+    public static MyPoint add(MyPoint p, MyPoint o)
+    {
+        return new MyPoint(p.x + o.x, p.y + o.y);
+    }
+
+    public static MyPoint clone(MyPoint p)
+    {
+        return new MyPoint(p.x, p.y);
+    }
+
+
+    public static MyPoint zero
+    {
+        get { return new MyPoint(0, 0); }
+    }
+    public static MyPoint one
+    {
+        get { return new MyPoint(1, 1); }
+    }
+    public static MyPoint up
+    {
+        get { return new MyPoint(0, 1); }
+    }
+    public static MyPoint down
+    {
+        get { return new MyPoint(0, -1); }
+    }
+    public static MyPoint right
+    {
+        get { return new MyPoint(1, 0); }
+    }
+    public static MyPoint left
+    {
+        get { return new MyPoint(-1, 0); }
+    }
+}
---
+++ HexPuzzle/Assets/Scipt/PieceController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PieceController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public int value;
+    public MyPoint piecePoint;
+
+    [HideInInspector]
+    public Vector2 pos;
+    [HideInInspector]
+    public RectTransform rect;
+
+    bool updating;
+
+    public void Initialize(int v, MyPoint p)
+    {
+        rect = GetComponent<RectTransform>();
+
+        value = v;
+        SetIndex(p);
+        SetColor(value);
+    }
+
+    public void SetColor(int val)
+    {
+        switch (val)
+        {
+            case (int)eStatus.NULL:
+                break;
+            case (int)eStatus.RED:
+                GetComponent<Image>().color = Color.red;
+                break;
+            case (int)eStatus.GREEN:
+                GetComponent<Image>().color = Color.green;
+                break;
+            case (int)eStatus.BLUE:
+                GetComponent<Image>().color = Color.blue;
+                break;
+            case (int)eStatus.MAGENTA:
+                GetComponent<Image>().color = Color.magenta;
+                break;
+            case (int)eStatus.YELLOW:
+                GetComponent<Image>().color = Color.yellow;
+                break;
+            case (int)eStatus.SILVER:
+                break;
+        }
+    }
+
+    public void SetIndex(MyPoint p)
+    {
+        piecePoint = p;
+        ResetPosition();
+        UpdateName();
+    }
+
+    public void ResetPosition()
+    {
+        pos = new Vector2(-360 + (120 * piecePoint.x), -400 - (70 * piecePoint.y));
+    }
+
+    public void MovePosition(Vector2 move)
+    {
+        rect.anchoredPosition += move * Time.deltaTime * 16f;
+    }
+
+    public void MovePositionTo(Vector2 move)
+    {
+        rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, move, Time.deltaTime * 16f);
+    }
+
+    public bool UpdatePiece()
+    {
+        if (Vector3.Distance(rect.anchoredPosition, pos) > 1)
+        {
+            MovePositionTo(pos);
+            updating = true;
+            return true;
+        }
+        else
+        {
+            rect.anchoredPosition = pos;
+            updating = false;
+            return false;
+        }
+    }
+
+    void UpdateName()
+    {
+        transform.name = "Node [" + piecePoint.x + ", " + piecePoint.y + "]";
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (updating) return;
+        GameManager.instance.MovePiece(this);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        GameManager.instance.DropPiece();
+    }
+}
